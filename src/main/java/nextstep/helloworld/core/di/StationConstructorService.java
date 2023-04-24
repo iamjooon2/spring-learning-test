bc28db6a33c67f4ff41de57be0735f1c1fc86cbe
iamjooon2 <iamjooon2@gmail.com>
2023-04-24 20:15:53 +0900

    Service layer final 파라미터 추가

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class StationConstructorService {
 
-    private StationRepository stationRepository;
+    private final StationRepository stationRepository;
 
     public StationConstructorService(final StationRepository stationRepository) {
         this.stationRepository = stationRepository;
